4b10e7c57af432ddce89b2c6d02e1a59171179e7
shenyiling <jzx19951028yyjy@163.com>
2019-03-25 13:03:22 +0800

    feat: login page ui

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -1,10 +1,56 @@
 import * as React from 'react'
+import {
+    Form,
+    Input,
+    Button
+} from 'antd'
+import { Link } from 'dva/router'
+import styles from './login.less'
 
-export default class Login extends React.Component {
+const FormItem = Form.Item
+
+export class Login extends React.Component<any, any> {
 
     render() {
-        return <div>
-            login page
-        </div>
+        const {
+            getFieldDecorator
+        } = this.props.form
+
+        return (
+            <main className={styles.container}>
+                <div className={styles.form__wrapper}>
+                    <Form className={styles.form}>
+                        <FormItem>
+                            {
+                                getFieldDecorator('userName', {})(
+                                    <Input 
+                                        placeholder="user name"
+                                    />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('password', {})(
+                                    <Input 
+                                        placeholder="password"
+                                    />
+                                )
+                            }
+                        </FormItem>
+                        <Button className={styles.submitBtn}>LOGIN</Button>
+                        <p>
+                            Not registered?
+                            <Link 
+                                className={styles.signUpLink} 
+                                to="./user/register"
+                                >Create an account</Link>
+                        </p>
+                    </Form>
+                </div>
+            </main>
+        )
     }
 }
+
+export default Form.create()(Login)
